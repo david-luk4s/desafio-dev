5ce46fedb3f0d0f01bd7ea092573c328b6c815db
David Lucas <davidlucadl99@gmail.com>
2023-03-20 22:24:01 -0300

    feat: call server handle

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/david-luk4s/desafio-dev/adapters/interfaces/api"
 	"github.com/david-luk4s/desafio-dev/config/database"
 )
 
 func main() {
 	// 	//init connection
 	database.ConnectionDB()
+
+	//start server
+	api.Handler()
 }
